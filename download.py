df57dd4294eff7d2d903be13145e1b84616b78f3
rmastand <radha.mastandrea@gmail.com>
2018-01-30 14:39:15 -0500

    Update download.py

--- download.py
+++ download.py
@@ -26,20 +26,20 @@ def download_root_files(list_of_downloads_file, destination_dir):
     files_to_download = f.read().split("\n")
 
     i = 1
-    '''
+   
     for root_link in files_to_download:
         print "Downloading file #" + str(i)
         http_link = root_link.replace(
-            "root://eospublic.cern.ch//", "http://opendata.cern.ch/")
+            "root://eospublic.cern.ch//", "http://eospublichttp.cern.ch/")
         root_file_name = http_link[78:len(http_link)]
         destination_dir_path = destination_dir + root_link[26:79]
 
-        call(["wget", "--continue", http_link, "-N", "-P", destination_dir_path])
+        call(["wget", "--continue", http_link, "-N", "-P", destination_dir_path,"--no-check-certificate"])
 
         i += 1
         print "\n"*5
-     '''
     
+    '''
     for root_link in files_to_download:
         print "Downloading file #"+ str(i)
 
@@ -47,7 +47,7 @@ def download_root_files(list_of_downloads_file, destination_dir):
         call(["xrdcp",root_link,destination_dir_path])
         i += 1
         print "\n"*5
-
+    '''
 
 
 download_root_files(list_of_downloads_file, destination_dir)
