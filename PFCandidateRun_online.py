e6b7f9b3e785b026b14ddf822a8de1efc3d0ea65
rmastand <radha.mastandrea@gmail.com>
2018-02-02 12:06:56 -0500

    Update PFCandidateRun_online.py

--- PFCandidateRun_online.py
+++ PFCandidateRun_online.py
@@ -38,13 +38,16 @@ readFiles.extend(files_to_process)
 process = cms.Process("MITCMSOpenData")
 
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = 'GR_R_42_V25::All'
+
+process.GlobalTag.connect = cms.string('sqlite_file:/cvmfs/cms-opendata-conddb.cern.ch/FT_53_LV5_AN1_RUNA.db')
+process.GlobalTag.globaltag = 'FT_53_LV5_AN1::All'
+
 
 process.source = cms.Source("PoolSource", fileNames=readFiles)
 
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 
-goodJSON = "file_paths/Cert_136033-149442_7TeV_Apr21ReReco_Collisions10_JSON_v2.txt"
+goodJSON = "file_paths/Cert_160404-180252_7TeV_ReRecoNov08_Collisions11_JSON.txt"
 myLumis = LumiList.LumiList(filename = goodJSON).getCMSSWString().split(',')
 process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange()
 process.source.lumisToProcess.extend(myLumis)
