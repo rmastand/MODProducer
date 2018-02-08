32020a5bd945c50cd5ab7e38561cbc56e8ce158f
rmastand <radha.mastandrea@gmail.com>
2018-02-08 15:14:07 -0500

    Update count_events.py

--- count_events.py
+++ count_events.py
@@ -1,4 +1,4 @@
 import sys
 file_to_count = sys.argv[1]
-print sum([int(x) for x in open(file_to_count).read().split()[::2]])
+print sum([int(x) for x in open(file_to_count).read().split()[1::2]])
 
