144426f5c377f967faf1332cc1557e71209ea786
rmastand <radha.mastandrea@gmail.com>
2018-03-08 13:15:12 -0500

    Update FilenameRun.py

--- reg/FilenameRun.py
+++ reg/FilenameRun.py
@@ -20,15 +20,13 @@ process.source = cms.Source ("PoolSource", fileNames=cms.untracked.vstring( data
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 
 if (data_type == "real") and (data_year == "2010"):
-	goodJSON = "file_paths/Cert_136033-149442_7TeV_Apr21ReReco_Collisions10_JSON_v2.txt"
+	goodJSON = "data/Cert_136033-149442_7TeV_Apr21ReReco_Collisions10_JSON_v2.txt"
 	myLumis = LumiList.LumiList(filename = goodJSON).getCMSSWString().split(',')
 	process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange()
 	process.source.lumisToProcess.extend(myLumis)
 
 if (data_type == "real") and (data_year == "2011"):
-	
-	
-	goodJSON = "file_paths/Cert_160404-180252_7TeV_ReRecoNov08_Collisions11_JSON.txt"
+	goodJSON = "data/Cert_160404-180252_7TeV_ReRecoNov08_Collisions11_JSON.txt"
 	myLumis = LumiList.LumiList(filename = goodJSON).getCMSSWString().split(',')
 	process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange()
 	process.source.lumisToProcess.extend(myLumis)
