1169b75b92418c80671dacedfcffdce6ba9733e8
rmastand <radha.mastandrea@gmail.com>
2018-05-15 11:22:35 -0400

    Update PFCandidateRun_online.py

--- analysis/PFCandidateRun_online.py
+++ analysis/PFCandidateRun_online.py
@@ -53,18 +53,18 @@ process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(20))
 
 if (data_type == "Data") and (data_year == "2010B"):
 	process.GlobalTag.globaltag='GR_R_42_V25::All'
-	goodJSON = "data/Cert_136033-149442_7TeV_Apr21ReReco_Collisions10_JSON_v2.txt"
-	myLumis = LumiList.LumiList(filename = goodJSON).getCMSSWString().split(',')
-	process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange()
-	process.source.lumisToProcess.extend(myLumis)
+	#goodJSON = "data/Cert_136033-149442_7TeV_Apr21ReReco_Collisions10_JSON_v2.txt"
+	#myLumis = LumiList.LumiList(filename = goodJSON).getCMSSWString().split(',')
+	#process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange()
+	#process.source.lumisToProcess.extend(myLumis)
 
 if (data_type == "Data") and (data_year == "2011A"):
 	process.GlobalTag.connect = cms.string('sqlite_file:/cvmfs/cms-opendata-conddb.cern.ch/FT_53_LV5_AN1_RUNA.db')
 	process.GlobalTag.globaltag = 'FT_53_LV5_AN1::All'
-	goodJSON = "data/Cert_160404-180252_7TeV_ReRecoNov08_Collisions11_JSON.txt"
-	myLumis = LumiList.LumiList(filename = goodJSON).getCMSSWString().split(',')
-	process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange()
-	process.source.lumisToProcess.extend(myLumis)
+	#goodJSON = "data/Cert_160404-180252_7TeV_ReRecoNov08_Collisions11_JSON.txt"
+	#myLumis = LumiList.LumiList(filename = goodJSON).getCMSSWString().split(',')
+	#process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange()
+	#process.source.lumisToProcess.extend(myLumis)
 
 if (data_type == "Sim") and (data_year == "2011"):
 	process.GlobalTag.connect = cms.string('sqlite_file:/cvmfs/cms-opendata-conddb.cern.ch/START53_LV6A1.db')
