ac46b953a36094057fbcb5d0e31fb92c8a97ef18
rmastand <radha.mastandrea@gmail.com>
2018-05-31 19:04:41 -0400

    Create skim_lumibyls.py

---
+++ reg/skim_lumibyls.py
@@ -0,0 +1,31 @@
+import sys
+runbyls_file = sys.argv[1]
+output_file = sys.argv[2]
+
+def runs_to_lumi(input,output):
+    """makes nicer file linking runs to lumi from file"""
+    runalumi_data = open(input)
+    output_data = open(output,"w")
+    lines =  runalumi_data.readlines()
+    split_lines = [line.split(",") for line in lines][2:]
+
+    char = ""
+    run_lumi_dict = {}
+    i = 0
+    while char !="#":
+    	run = split_lines[i][0].strip().split(":")[0]
+        lumi = split_lines[i][1].strip().split(":")[0]
+    	rlumi_delivered = float(split_lines[i][5])
+    	rlumi_recorded = float(split_lines[i][6])
+
+        output_data.write(str(run)+"_"+str(lumi)+" "+str(rlumi_delivered)+" "+str(rlumi_recorded)+"\n")
+
+
+    	i += 1
+    	try:
+    		char = split_lines[i][0][0]
+    	except: pass
+    return run_lumi_dict
+
+
+runs_to_lumi(runbyls_file,output_file)
