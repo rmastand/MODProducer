5a9c36c6fc3c0ad2e57e0051aa2a622ef14fdd4e
rmastand <radha.mastandrea@gmail.com>
2018-06-01 15:31:32 -0400

    Add files via upload

---
+++ post/stats_to_mod.py
@@ -0,0 +1,78 @@
+import sys
+import numpy as np
+import gc
+gc.collect()
+mod_orig = sys.argv[1]
+lumibyls = sys.argv[2]
+mod_file = open(mod_orig)
+mod_lines = mod_file.readlines()
+mod_file.close()
+
+
+
+def runs_to_lumi(filename):
+	lumibyls_data = open(filename)
+	lines = lumibyls_data.readlines()
+	split_lines = [line.split(",") for line in lines][2:]
+	
+	char = ""
+	run_lumi_dict =  {}
+	i = 0
+	while char != "#":
+		run = split_lines[i][0].strip().split(":")[0]
+		lumi = split_lines[i][1].strip().split(".")[0]
+		rlumi_delivered = float(split_lines[i][5])
+		rlumi_recorded = float(split_lines[i][6])
+		run_lumi_dict[(run,lumi)] = (rlumi_delivered,rlumi_recorded)
+
+		i += 1
+		try:
+			char = split_lines[i][0][0]
+		except: pass
+	return run_lumi_dict		
+
+
+
+
+lumi_info = {}
+total_events = 0
+valid_events = 0
+run_lumi_dict = runs_to_lumi(lumibyls)
+total_lum_del = 0.0
+total_lum_rec = 0.0
+
+
+for i in xrange(len(mod_lines)):
+	if ("#" in mod_lines[i].split()) and ("Cond" in mod_lines[i].split()):
+		run = mod_lines[i+1].split()[1]
+		lumiBlock = mod_lines[i+1].split()[6]
+		valid = mod_lines[i+1].split()[7]
+		if (run,lumiBlock) not in lumi_info.keys():
+			lumi_info[(run,lumiBlock)] = {"events":1,"valid":valid}
+		else:
+			lumi_info[(run,lumiBlock)]["events"] += 1
+		total_events += 1
+		valid_events += int(valid)
+		try:
+			total_lum_del += run_lumi_dict[(run,lumiBlock)][0]
+			total_lum_rec += run_lumi_dict[(run,lumiBlock)][1]
+		except KeyError:
+			pass
+
+def format2_6(string,num):
+	return " "*(num-len(string))+ string
+
+
+w = open("stats2.txt","w")
+w.write("BeginFile \n")
+w.write("#   File"+format2_6("Filename",40)+format2_6("TotalEvents",15)+format2_6("ValidEvents",15)+format2_6("IntLumiDel",20)+format2_6("IntLumiRec",20)+"\n")
+w.write("    File"+format2_6(str(mod_orig[-40:-4]),40)+format2_6(str(total_events),15)+format2_6(str(valid_events),15)+format2_6(str(total_lum_del),20)+format2_6(str(total_lum_rec),20)+"\n")
+
+
+w.write("#LumiBlock"+format2_6("RunNum",15)+format2_6("Lumi",10)+format2_6("Events",10)+format2_6("Valid?",10)+format2_6("IntLumiDel",20)+format2_6("IntLumiRec",20)+"\n")
+for lumi in sorted(sorted(lumi_info.keys(),key=lambda tup: tup[1]),key=lambda tup: tup[0]):
+	try:
+		w.write(" LumiBlock"+format2_6(str(lumi[0]),15)+format2_6(str(lumi[1]),10)+format2_6(str(lumi_info[lumi]["events"]),10)+format2_6(str(lumi_info[lumi]["valid"]),10)+format2_6(str(run_lumi_dict[lumi][0]),20)+format2_6(str(run_lumi_dict[lumi][1]),20)+"\n")
+	except KeyError:
+		w.write(" LumiBlock"+format2_6(str(lumi[0]),15)+format2_6(str(lumi[1]),10)+format2_6(str(lumi_info[lumi]["events"]),10)+format2_6(str(lumi_info[lumi]["valid"]),10)+format2_6("0.0",20)+format2_6("0.0",20)+"\n")
+	
