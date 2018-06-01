877bd51b0bce40f7fd7b8e6c300379b5cd25a66f
rmastand <radha.mastandrea@gmail.com>
2018-06-01 17:02:37 -0400

    Update stats_to_mod.py

--- post/stats_to_mod.py
+++ post/stats_to_mod.py
@@ -6,6 +6,7 @@ gc.collect()
 
 mod_orig = sys.argv[1]
 lumibyls = sys.argv[2]
+input_dir = sys.argv[3]
 version = "6"
 data_year = "2011A"
 data_type = "Data"
@@ -47,38 +48,41 @@ total_lum_del = 0.0
 total_lum_rec = 0.0
 
 
-with open(mod_orig) as mod_file:
-    for line in mod_file:
-	if ("#" not in line.split()) and ("Cond" in line.split()):
-		run = line.split()[1]
-		lumiBlock = line.split()[6]
-		
-		if (run,lumiBlock) not in lumi_info.keys():
-			lumi_info[(run,lumiBlock)] = {"events":1,"valid":0}
-		else:
-			lumi_info[(run,lumiBlock)]["events"] += 1
-		total_events += 1
-		
+for mod_orig in os.listdir(input_dir):
+# counters for what to write to stats2
+	with open(mod_orig, "rb") as mod_file:
+	    for line in mod_file:
+		if ("#" not in line.split()) and ("Cond" in line.split()):
+			run = line.split()[1]
+			lumiBlock = line.split()[6]
+
+			if (run,lumiBlock) not in lumi_info.keys():
+				lumi_info[(run,lumiBlock)] = {"events":1,"valid":0}
+			else:
+				lumi_info[(run,lumiBlock)]["events"] += 1
+			total_events += 1
+
+			try:
+				total_lum_del += run_lumi_dict[(run,lumiBlock)][0]
+				total_lum_rec += run_lumi_dict[(run,lumiBlock)][1]
+				lumi_info[(run,lumiBlock)]["valid"] = 1
+				valid_events += 1
+			except KeyError:
+				pass
+	mod_file.close()
+
+	# actually writes stats2
+	w = open(input_dir.replace("MOD","stats2")+"/"+str(mod_orig[-40:-4])+".stats2","w")
+	w.write("BeginFile Version " + version + " CMS_" + data_year + " " + data_type + " " + trigger_cat + "\n")
+	w.write("#   File"+format2_6("Filename",40)+format2_6("TotalEvents",15)+format2_6("ValidEvents",15)+format2_6("IntLumiDel",20)+format2_6("IntLumiRec",20)+"\n")
+	w.write("    File"+format2_6(str(mod_orig[-40:-4]),40)+format2_6(str(total_events),15)+format2_6(str(valid_events),15)+format2_6(str(total_lum_del),20)+format2_6(str(total_lum_rec),20)+"\n")
+
+
+	w.write("#LumiBlock"+format2_6("RunNum",15)+format2_6("Lumi",10)+format2_6("Events",10)+format2_6("Valid?",10)+format2_6("IntLumiDel",20)+format2_6("IntLumiRec",20)+"\n")
+	for lumi in sorted(sorted(lumi_info.keys(),key=lambda tup: tup[1]),key=lambda tup: tup[0]):
 		try:
-			total_lum_del += run_lumi_dict[(run,lumiBlock)][0]
-			total_lum_rec += run_lumi_dict[(run,lumiBlock)][1]
-			lumi_info[(run,lumiBlock)]["valid"] = 1
-			valid_events += 1
+			w.write(" LumiBlock"+format2_6(str(lumi[0]),15)+format2_6(str(lumi[1]),10)+format2_6(str(lumi_info[lumi]["events"]),10)+format2_6(str(lumi_info[lumi]["valid"]),10)+format2_6(str(run_lumi_dict[lumi][0]),20)+format2_6(str(run_lumi_dict[lumi][1]),20)+"\n")
 		except KeyError:
-			pass
-mod_file.close()
-
-
-w = open("stats2.txt","w")
-w.write("BeginFile Version " + version + " CMS_" + data_year + " " + data_type + " " + trigger_cat + "\n")
-w.write("#   File"+format2_6("Filename",40)+format2_6("TotalEvents",15)+format2_6("ValidEvents",15)+format2_6("IntLumiDel",20)+format2_6("IntLumiRec",20)+"\n")
-w.write("    File"+format2_6(str(mod_orig[-40:-4]),40)+format2_6(str(total_events),15)+format2_6(str(valid_events),15)+format2_6(str(total_lum_del),20)+format2_6(str(total_lum_rec),20)+"\n")
-
-
-w.write("#LumiBlock"+format2_6("RunNum",15)+format2_6("Lumi",10)+format2_6("Events",10)+format2_6("Valid?",10)+format2_6("IntLumiDel",20)+format2_6("IntLumiRec",20)+"\n")
-for lumi in sorted(sorted(lumi_info.keys(),key=lambda tup: tup[1]),key=lambda tup: tup[0]):
-	try:
-		w.write(" LumiBlock"+format2_6(str(lumi[0]),15)+format2_6(str(lumi[1]),10)+format2_6(str(lumi_info[lumi]["events"]),10)+format2_6(str(lumi_info[lumi]["valid"]),10)+format2_6(str(run_lumi_dict[lumi][0]),20)+format2_6(str(run_lumi_dict[lumi][1]),20)+"\n")
-	except KeyError:
-		w.write(" LumiBlock"+format2_6(str(lumi[0]),15)+format2_6(str(lumi[1]),10)+format2_6(str(lumi_info[lumi]["events"]),10)+format2_6(str(lumi_info[lumi]["valid"]),10)+format2_6("0.0",20)+format2_6("0.0",20)+"\n")
-w.write("EndFile")
+			w.write(" LumiBlock"+format2_6(str(lumi[0]),15)+format2_6(str(lumi[1]),10)+format2_6(str(lumi_info[lumi]["events"]),10)+format2_6(str(lumi_info[lumi]["valid"]),10)+format2_6("0.0",20)+format2_6("0.0",20)+"\n")
+	w.write("EndFile")
+	w.close()
