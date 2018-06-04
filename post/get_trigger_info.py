e16ffaf9cee113da367c0e747f864ffa8020f2a1
rmastand <radha.mastandrea@gmail.com>
2018-06-04 16:21:19 -0400

    Update get_trigger_info.py

--- post/get_trigger_info.py
+++ post/get_trigger_info.py
@@ -4,7 +4,7 @@ import os
 mod_file_dir = sys.argv[1]
 skimmed_lumibyls = sys.argv[2]
 
-trig_dict = {}
+
 
 def get_lumiId_to_lumin(skim_filename):
 	lumiId_to_lumin_dict = {}
@@ -26,6 +26,7 @@ lumiId_to_lumin_dict = get_lumiId_to_lumin(skimmed_lumibyls)
 
 
 for file in os.listdir(mod_file_dir):
+	trig_dict = {}
 	with open(mod_file_dir+"/"+file, "rb") as mod_file:
 		for line in mod_file: 
 			# keeps track of the run, lumiBlock
@@ -37,14 +38,17 @@ for file in os.listdir(mod_file_dir):
 				# given line: [Trig identifier, trig name, prescale1, prescale2, fired?]
 				if line.split()[1] not in trig_dict.keys():
 					trig_dict[line.split()[1]] = {"present":1,"present_valid":is_lumi_valid((run,lumiBlock),lumiId_to_lumin_dict),"present_fired":int(line.split()[4]),"present_valid_fired":is_lumi_valid((run,lumiBlock),lumiId_to_lumin_dict) and int(line.split()[4])}
-					print trig_dict[line.split()[1]]
+					
 				else:
 					trig_dict[line.split()[1]]["present"] += 1
 					trig_dict[line.split()[1]]["present_valid"] += is_lumi_valid((run,lumiBlock),lumiId_to_lumin_dict)
 					trig_dict[line.split()[1]]["present"] += int(line.split()[4])
 					trig_dict[line.split()[1]]["present"] += is_lumi_valid((run,lumiBlock),lumiId_to_lumin_dict) and int(line.split()[4])
-for trig in trig_dict.keys():
-	print trig
-	print trig_dict[trig]
+	for trig in trig_dict.keys():
+		print trig
+		print trig_dict[trig]
+		print
+	print 
+	print
 	print
 
