ce1187093f8673c9ad7468a01a51b185c622820e
rmastand <radha.mastandrea@gmail.com>
2018-06-04 15:51:38 -0400

    Update get_trigger_info.py

--- post/get_trigger_info.py
+++ post/get_trigger_info.py
@@ -6,8 +6,8 @@ mod_file_dir = sys.argv[1]
 trig_dict = {}
 
 for file in os.listdir(mod_file_dir):
-  with open(mod_file_dir+"/"+file, "rb") as mod_file:
-	    for line in mod_file: 
-        if ("Trig" in line.split()) and ("#" not in line.split()):
-          print line.split()
+	with open(mod_file_dir+"/"+file, "rb") as mod_file:
+		for line in mod_file: 
+			if ("Trig" in line.split()) and ("#" not in line.split()):
+				print line.split()
       
