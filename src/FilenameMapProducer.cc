ba264790abc13fd906b4e4295a2dc5a31de81255
rmastand <radha.mastandrea@gmail.com>
2018-06-06 10:36:54 -0400

    Update FilenameMapProducer.cc

--- src/FilenameMapProducer.cc
+++ src/FilenameMapProducer.cc
@@ -19,7 +19,9 @@
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/InputTag.h"
-
+#include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
+#include "SimDataFormats/GeneratorProducts/interface/GenRunInfoProduct.h"
+#include "SimDataFormats/PileupSummaryInfo/interface/PileupSummaryInfo.h"
 
 
 
@@ -33,6 +35,7 @@ int validEvents = 0;
 long double intLumiTotDel = 0.;
 long double intLumiTotRec = 0.;
 list<string> usedLumis;
+double crossSection;
 
 class FilenameMapProducer : public EDProducer 
 {
@@ -91,23 +94,25 @@ FilenameMapProducer::FilenameMapProducer(const ParameterSet& iConfig)
   statsFilename_ = outputDir_ + "/" + currentProcessingFilename_.substr(0,currentProcessingFilename_.length()-5) + ".stats";
 
      
-   
-  lumiLumin_.open(outputLsFile_.c_str());
-  int line_number = 1;
+  if (dataType_ == "Data) 
+      {
+	  lumiLumin_.open(outputLsFile_.c_str());
+	  int line_number = 1;
 
-  string line;
-  while (getline(lumiLumin_, line)) {
-      
-      istringstream iss(line);
-      string lumiId; 
-      long double lumiDel, lumiRec;
+	  string line;
+	  while (getline(lumiLumin_, line)) {
 
-      iss >> lumiId >> lumiDel >> lumiRec;     
-      lumiDelData[lumiId]=lumiDel;
-      lumiRecData[lumiId]=lumiRec;   
+	      istringstream iss(line);
+	      string lumiId; 
+	      long double lumiDel, lumiRec;
 
-      
-      line_number++;
+	      iss >> lumiId >> lumiDel >> lumiRec;     
+	      lumiDelData[lumiId]=lumiDel;
+	      lumiRecData[lumiId]=lumiRec;   
+
+
+	      line_number++;
+  }
    }
 
 
@@ -128,9 +133,10 @@ void FilenameMapProducer::produce(Event& iEvent, const EventSetup& iSetup) {
    int eventNum = iEvent.id().event();
    int lumiBlock = iEvent.luminosityBlock();
 
+   // registry file output
    fileOutput_ << eventNum << " " << runNum  << " " << currentProcessingFilename_ << endl;
    
-   
+   // easy map between lumi id to lumi run, lumi block
    lumiToRun[std::to_string(runNum)+"_"+std::to_string(lumiBlock)] = std::to_string(runNum);
    lumiToLumiB[std::to_string(runNum)+"_"+std::to_string(lumiBlock)] = std::to_string(lumiBlock);
    
@@ -143,20 +149,25 @@ void FilenameMapProducer::produce(Event& iEvent, const EventSetup& iSetup) {
       }
    ++totEvents;
    
-   if (lumiDelData.count(std::to_string(runNum)+"_"+std::to_string(lumiBlock))==1) {
-      ++validEvents;
-	   
-      if (std::find(std::begin(usedLumis), std::end(usedLumis), std::to_string(runNum)+"_"+std::to_string(lumiBlock)) != std::end(usedLumis))
-      {
-    	// my_list has my_var
-   
-      }
-	   else {
-		   usedLumis.push_front(std::to_string(runNum)+"_"+std::to_string(lumiBlock));
-		   intLumiTotDel = intLumiTotDel + lumiDelData[std::to_string(runNum)+"_"+std::to_string(lumiBlock)];
-      		   intLumiTotRec = intLumiTotRec + lumiRecData[std::to_string(runNum)+"_"+std::to_string(lumiBlock)];
+   // on the real data: checks for valid events, keeps counter of integrated luminosity
+   if (dataType_ == "Data"){
+	   if (lumiDelData.count(std::to_string(runNum)+"_"+std::to_string(lumiBlock))==1) {
+	      ++validEvents;
+
+	      if (std::find(std::begin(usedLumis), std::end(usedLumis), std::to_string(runNum)+"_"+std::to_string(lumiBlock)) != std::end(usedLumis))
+	      {
+		// my_list has my_var
+
+	      }
+		   else {
+			   usedLumis.push_front(std::to_string(runNum)+"_"+std::to_string(lumiBlock));
+			   intLumiTotDel = intLumiTotDel + lumiDelData[std::to_string(runNum)+"_"+std::to_string(lumiBlock)];
+			   intLumiTotRec = intLumiTotRec + lumiRecData[std::to_string(runNum)+"_"+std::to_string(lumiBlock)];
+		   }
 	   }
+
    }
+   
 
    
    
@@ -177,8 +188,9 @@ void FilenameMapProducer::endJob() {
    
 
 
-   
-   statsOutput_ << "#   File                                Filename    TotalEvents    ValidEvents          IntLumiDel          IntLumiRec" << endl;
+   if (dataType_ == "Data") {
+	   
+	   statsOutput_ << "#   File                                Filename    TotalEvents    ValidEvents          IntLumiDel          IntLumiRec" << endl;
    string LumiTotDel = std::to_string(intLumiTotDel);
    LumiTotDel.erase(LumiTotDel.find_last_not_of("0")+1,std::string::npos);
    string LumiTotRec = std::to_string(intLumiTotRec);
@@ -190,8 +202,29 @@ void FilenameMapProducer::endJob() {
 		<< setw(20) << LumiTotDel 
 		<< setw(20) << LumiTotRec
 	 	<< endl;   
+	   
+	   
+	   
+   }
 	
-   statsOutput_ << "#LumiBlock         RunNum      Lumi    Events    Valid?     IntLumiDel     IntLumiRec" << endl;
+   if (dataType_ == "Sim") {
+	   
+	   statsOutput_ << "#   File                                Filename    TotalEvents    ValidEvents        CrossSection" << endl;
+   
+	   statsOutput_ <<fixed<< "    File"
+			<< setw(40) << currentProcessingFilename_.substr(0,currentProcessingFilename_.length()-5)
+			<< setw(15) << totEvents
+			<< setw(15) << validEvents
+			<< setw(20) << crossSection 
+			<< endl;   
+	   
+	   
+	   
+   }
+   
+   if (dataType_ == "Data") {
+	   
+	   statsOutput_ << "#LumiBlock         RunNum      Lumi    Events    Valid?     IntLumiDel     IntLumiRec" << endl;
 	
    std::map<std::string, int>::iterator it = lumiNumEvents.begin();
    while (it != lumiNumEvents.end())
@@ -231,6 +264,42 @@ void FilenameMapProducer::endJob() {
  
            
             }
+	   
+	   
+   }
+	
+	
+      if (dataType_ == "Sim") {
+	   
+	   statsOutput_ << "#LumiBlock         RunNum      Lumi    Events    Valid?        CrossSection" << endl;
+	
+   std::map<std::string, int>::iterator it = lumiNumEvents.begin();
+   while (it != lumiNumEvents.end())
+  
+            {
+            string k =  it->first;  
+	   
+	    
+	   
+		statsOutput_ <<" LumiBlock"
+			     << setw(15) << lumiToRun[k]
+			     << setw(10) << lumiToLumiB[k]
+			     << setw(10) << lumiNumEvents[k]
+			     << setw(10) << "-1"
+			     << setw(20) << crossSection
+			     << endl;   
+		    
+	   
+     		
+	      ++it;
+ 
+           
+            }
+	   
+	   
+   }
+	
+   
 
  
  	
@@ -242,6 +311,15 @@ void FilenameMapProducer::endJob() {
 }
 
 void FilenameMapProducer::beginRun(edm::Run & iRun, edm::EventSetup const & iSetup){
+	
+	if (dataType_=="Sim") {
+        edm::Handle<GenRunInfoProduct> genRunInfo;
+        iRun.getByLabel("generator", genRunInfo );
+
+
+        crossSection = genRunInfo->crossSection();
+
+	}
 
 }
 
