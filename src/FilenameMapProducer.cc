942b9d5cd7e3075dc7bbe429ad7741b68a139843
rmastand <radha.mastandrea@gmail.com>
2018-06-06 11:03:41 -0400

    Update FilenameMapProducer.cc

--- src/FilenameMapProducer.cc
+++ src/FilenameMapProducer.cc
@@ -78,9 +78,7 @@ private:
 
 
 FilenameMapProducer::FilenameMapProducer(const ParameterSet& iConfig)
-: hltConfig_(),
-  hltInputTag_("TriggerResults","","HLT"),
-  currentProcessingFilename_(iConfig.getParameter<string>("filename")),
+: currentProcessingFilename_(iConfig.getParameter<string>("filename")),
   outputFilename_(iConfig.getParameter<string>("outputFile")),
   outputLsFile_(iConfig.getParameter<string>("outputLsFile")),
   outputDir_(iConfig.getParameter<string>("outputDir")),
