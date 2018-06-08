99beba9a917c3645a40119e38ec84ab97169ab87
rmastand <radha.mastandrea@gmail.com>
2018-06-07 21:46:51 -0400

    Update mod_to_stats.py

--- post/mod_to_stats.py
+++ post/mod_to_stats.py
@@ -109,12 +109,12 @@ for mod_orig in os.listdir(input_dir):
 	if data_type == "Data":
 		w.write("#   File"+format2_6("Filename",40)+format2_6("TotalEvents",15)+format2_6("ValidEvents",15)+format2_6("IntLumiDel",15)+format2_6("IntLumiRec",15)+"\n")
 		w.write("    File"+format2_6(str(mod_orig[-40:-4]),40)+format2_6(str(total_events),15)+format2_6(str(valid_events),15)+format2_6("{0:.3f}".format(total_lum_del),15)+format2_6("{0:.3f}".format(total_lum_rec),15)+"\n")
-		w.write("#  Block"+format2_6("RunNum",15)+format2_6("LumiBlock",10)+format2_6("Events",10)+format2_6("Valid?",10)+format2_6("IntLumiDel",15)+format2_6("IntLumiRec",15)+"\n")
+		w.write("#  Block"+format2_6("RunNum",22)+format2_6("LumiBlock",1180)+format2_6("Events",15)+format2_6("Valid?",15)+format2_6("IntLumiDel",15)+format2_6("IntLumiRec",15)+"\n")
 		for lumi in sorted(sorted(lumi_info.keys(),key=lambda tup: tup[1]),key=lambda tup: tup[0]):
 			try:
-				w.write("   Block"+format2_6(str(lumi[0]),15)+format2_6(str(lumi[1]),10)+format2_6(str(lumi_info[lumi]["events"]),10)+format2_6(str(lumi_info[lumi]["valid"]),10)+format2_6("{0:.3f}".format(run_lumi_dict[lumi][0]),15)+format2_6("{0:.3f}".format(run_lumi_dict[lumi][1]),15)+"\n")
+				w.write("   Block"+format2_6(str(lumi[0]),22)+format2_6(str(lumi[1]),18)+format2_6(str(lumi_info[lumi]["events"]),15)+format2_6(str(lumi_info[lumi]["valid"]),15)+format2_6("{0:.3f}".format(run_lumi_dict[lumi][0]),15)+format2_6("{0:.3f}".format(run_lumi_dict[lumi][1]),15)+"\n")
 			except KeyError:
-				w.write("   Block"+format2_6(str(lumi[0]),15)+format2_6(str(lumi[1]),10)+format2_6(str(lumi_info[lumi]["events"]),10)+format2_6(str(lumi_info[lumi]["valid"]),10)+format2_6("0.000",15)+format2_6("0.000",15)+"\n")
+				w.write("   Block"+format2_6(str(lumi[0]),22)+format2_6(str(lumi[1]),18)+format2_6(str(lumi_info[lumi]["events"]),15)+format2_6(str(lumi_info[lumi]["valid"]),15)+format2_6("0.000",15)+format2_6("0.000",15)+"\n")
 	if data_type == "Sim":
 		w.write("#   File"+format2_6("Filename",40)+format2_6("TotalEvents",15)+format2_6("ValidEvents",15)+format2_6("CrossSection",15)+"\n")
 		w.write("    File"+format2_6(str(mod_orig[-40:-4]),40)+format2_6(str(total_events),15)+format2_6(str(total_events),15)+format2_6("{0:.3f}".format(float(cross_section)),15)+"\n")
