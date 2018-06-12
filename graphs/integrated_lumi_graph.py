25a7d455b576a5f901b00c5016c884951004b33c
rmastand <radha.mastandrea@gmail.com>
2018-06-12 13:51:22 -0400

    Update integrated_lumi_graph.py

--- graphs/integrated_lumi_graph.py
+++ graphs/integrated_lumi_graph.py
@@ -17,6 +17,7 @@ plt.rcParams['xtick.labelsize'] = 16
 plt.rcParams['ytick.labelsize'] = 16
 plt.rcParams['legend.fontsize'] = 16
 plt.rcParams['text.usetex'] = True
+plt.rcParams['text.latex.unicode']=True
 plt.rcParams['figure.facecolor'] = "white"
 
 
