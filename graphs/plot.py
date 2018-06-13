2feb93b20b7a288f74c4b72378f401f1b74d5fe4
rmastand <radha.mastandrea@gmail.com>
2018-06-13 13:46:56 -0400

    Update plot.py

--- graphs/plot.py
+++ graphs/plot.py
@@ -112,7 +112,7 @@ def graph_fired_over_eff_lumin():
 	
 	plt.figure(figsize=(12,10))
 	color_index = 0
-	x = 5
+	x = -80
 	trig_name_positions = {"HLT_Jet30":(x,300),"HLT_Jet60":(x,12),"HLT_Jet80":(x,3),
 			      "HLT_Jet110":(x,.5),"HLT_Jet150":(x,.12),"HLT_Jet190":(x,.04),
 			      "HLT_Jet240":(x,.01),"HLT_Jet300":(x,.003),"HLT_Jet370":(x,.0015)}
@@ -127,13 +127,13 @@ def graph_fired_over_eff_lumin():
 		color_index += 1
 
 	#plt.xlabel("Run:Lumiblock")
-	plt.xlabel("# pf Luminosity Blocks (time-ordered)")
+	plt.xlabel("# of Luminosity Blocks (time-ordered)")
 
 	#plt.xticks(range(len(lines[0].split(",")))[::id_spacing],lines[0].split(",")[::id_spacing], rotation=30)
 	plt.ylabel("Times Fired / Effective Luminosity (ub)")
 	plt.yscale("log")
 	ax = plt.gca()
-	ax.set_xlim(left = .3)
+	ax.set_xlim(left = -100)
 	box = ax.get_position()
 	ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
 
