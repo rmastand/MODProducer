135474cd71a5b8974391ea038496559f29b8c654
rmastand <radha.mastandrea@gmail.com>
2018-06-13 13:53:28 -0400

    Update plot.py

--- graphs/plot.py
+++ graphs/plot.py
@@ -88,9 +88,10 @@ def graph_eff_lumin():
 	#plt.xlabel("Run:LumiBlock")
 	#plt.xticks(range(len(time_ordered_lumi_id))[::id_spacing], time_ordered_lumi_id[::id_spacing], rotation=30)
 	ax = plt.gca()
+	plt.axis('scaled')
 	ax.set_xlim(left = .3)
 	plt.xlabel("# of Luminosity Blocks (time-ordered)")
-	plt.axis('scaled')
+	
 	ax.add_artist(logo_box())
 	plt.ylabel("Effective Luminosity " +"ub^{-1}")
 	plt.yscale("log")
