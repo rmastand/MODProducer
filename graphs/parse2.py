a991d2ba0035c85e62f8656a27433b3f8f9f1790
rmastand <radha.mastandrea@gmail.com>
2018-06-14 11:30:57 -0400

    Update parse2.py

--- graphs/parse2.py
+++ graphs/parse2.py
@@ -59,11 +59,10 @@ def get_file_trig_dict_from_txt(filepath):
 				fired_dict = {}
 				fired_info = line.split(",")
 				for j in range(len(fired_info)):
-					if fired_info[j] != "\n":
-						
+					if fired_info[j] != "\n":	
+						print fired_info[j]
 						run_lumi_id = fired_info[j].split(":")[0]
 						times_fired = int(fired_info[j].split(":")[1])
-						
 						fired_dict[(run_lumi_id.split("_")[0],run_lumi_id.split("_")[1])] = times_fired
 				
 				trig_dict[trigger_name]["fired"] = fired_dict
