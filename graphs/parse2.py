f9e41cf82de21b3dbf0d2d53a72c301e9b0ee272
rmastand <radha.mastandrea@gmail.com>
2018-06-14 11:32:51 -0400

    Update parse2.py

--- graphs/parse2.py
+++ graphs/parse2.py
@@ -61,6 +61,7 @@ def get_file_trig_dict_from_txt(filepath):
 				for j in range(len(fired_info)):
 					if fired_info[j] != "\n":	
 						print fired_info[j]
+						print fired_info[j].split(":")
 						run_lumi_id = fired_info[j].split(":")[0]
 						times_fired = int(fired_info[j].split(":")[1])
 						fired_dict[(run_lumi_id.split("_")[0],run_lumi_id.split("_")[1])] = times_fired
