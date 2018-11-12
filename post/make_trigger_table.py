4b668b58562b94a55097281fb862413316a71d00
rmastand <radha.mastandrea@gmail.com>
2018-11-12 00:46:56 -0500

    Update make_trigger_table.py

--- post/make_trigger_table.py
+++ post/make_trigger_table.py
@@ -8,22 +8,37 @@ import numpy as np
 
 trig_file_dir = sys.argv[1]
 
+#structure: trigger: (present,valid,fired)
+#contains a bonus item: total events = (present, valid)
+all_triggers_dict = {total_events = (0,0)}
+
 for file in os.listdir(mod_file_dir):
-	trig_dict = {}
-	tot_present = 0
-	tot_valid = 0
-	# we'll use this later for calculate the total delivered and recorded luminosities
-	good_lumis = []
+	with open(trig_file_dir+"/"+file, "r") as trig_file:
+		for line in trig_file:
+			if ("File" in line.split()) and ("#" not in line.split()):
+				all_triggers_dict[total_events][0] += line.split()[2]
+				all_triggers_dict[total_events][1] += line.split()[3]
+			# looks for each trigger event
+			if ("Trig" in line.split()) and ("#" not in line.split()):
+				master_info = line.split()
+				#ignore the version numbers for the trigger name
+				trigger_name = master_info[1][:-3]
+				present = int(master_info[2])
+				valid = int(master_info[3])
+				fired = int(master_info[4])
+				
+				# if we haven't seen the trigger before:
+				if trigger_name not in all_triggers_dict.keys():
+					all_triggers_dict[trigger_name] = (present, valid, fired)
+				else:
+					all_triggers_dict[trigger_name][0] += present
+					all_triggers_dict[trigger_name][1] += valid
+					all_triggers_dict[trigger_name][2] += fired
+				
+w = open("trigger_table.txt,"w")
+w.write("total "+str(all_triggers_dict[total_events][0])+" "+str(all_triggers_dict[total_events][1])+"\n")
+for trigger_name in all_triggers_dict.keys():
+	w.write(trigger_name + " " + str(all_triggers_dict[trigger_name][0])+" "+str(all_triggers_dict[trigger_name][1])+ " " + str(all_triggers_dict[trigger_name][2])+ "\n") 
+w.close()
 
-	with open(mod_file_dir+"/"+file, "rb") as mod_file:
-		for line in mod_file: 
-	
 
-			# keeps track of the run, lumiBlock
-			# this should signal each separate event
-			if (("Cond" in line.split()) or ("SCond" in line.split())) and ("#" not in line.split()):
-				run,lumiBlock = line.split()[1],line.split()[6]
-				tot_present += 1
-
-				if (is_lumi_valid((run,lumiBlock),lumiId_to_lumin_dict)) and (data_type == "Data"):
-					tot_valid += 1		
