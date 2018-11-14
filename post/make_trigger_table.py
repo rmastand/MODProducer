7893e0c31b2160b92059599f7d2e7e26ddb2b493
rmastand <radha.mastandrea@gmail.com>
2018-11-14 00:49:31 -0500

    Update make_trigger_table.py

--- post/make_trigger_table.py
+++ post/make_trigger_table.py
@@ -23,6 +23,8 @@ for trig_dir in all_trig_dirs:
 		with open(trig_dir+"/"+file, "r") as trig_file:
 			for line in trig_file:
 				if ("File" in line.split()) and ("#" not in line.split()):
+					print int(line.split()[2])
+					print all_triggers_dict["total_events"][0]
 					all_triggers_dict["total_events"][0] += int(line.split()[2])
 					all_triggers_dict["total_events"][1] += int(line.split()[3])
 				# looks for each trigger event
