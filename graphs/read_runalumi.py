b7bce5280e89d4883d72dfcaa423ad9da04c790c
rmastand <radha.mastandrea@gmail.com>
2018-11-17 21:54:24 -0500

    Create read_runalumi.py

---
+++ graphs/read_runalumi.py
@@ -0,0 +1,11 @@
+import sys
+import numpy as np
+
+run_alumi_file = sys.argv[1]
+
+read_alumi_lines = open(run_alumi_file,"r").readlines()
+for line in read_alumi_lines[4:]:
+  char = line[0]
+  while char != "#":
+    run = line.split()[1].split(":")[0]
+    print run
