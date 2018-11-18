54717d606b9546c2215f78fe7f79c4c571d3793a
rmastand <radha.mastandrea@gmail.com>
2018-11-17 21:55:21 -0500

    Update read_runalumi.py

--- graphs/read_runalumi.py
+++ graphs/read_runalumi.py
@@ -5,6 +5,7 @@ run_alumi_file = sys.argv[1]
 
 read_alumi_lines = open(run_alumi_file,"r").readlines()
 for line in read_alumi_lines[4:]:
+  print line
   char = line[0]
   while char != "#":
     run = line.split()[1].split(":")[0]
