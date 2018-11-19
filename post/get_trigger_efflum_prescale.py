e160496ce2e81ed8896d0d4ea154f9c710e9e370
rmastand <radha.mastandrea@gmail.com>
2018-11-18 19:09:17 -0500

    Update get_trigger_efflum_prescale.py

--- post/get_trigger_efflum_prescale.py
+++ post/get_trigger_efflum_prescale.py
@@ -7,7 +7,19 @@ import csv
 
 parsed_file_inpur_dir = sys.argv[1]
 lumibyls_file = sys.argv[2]
-output_file = sys.argv[3]
+run_alumi_file = sys.argv[3]
+output_file = sys.argv[4]
+
+runA_runs = []
+read_alumi_lines = open(run_alumi_file,"r").readlines()
+for line in read_alumi_lines[4:]:
+  char = line[0]
+  if char != "+":
+    runA_runs.append(line.split()[1].split(":")[0])
+  else:
+    break
+print "Done reading in ALumi"
+
 
 def setw(word,n):
 	return " "*(n-len(word))+word
