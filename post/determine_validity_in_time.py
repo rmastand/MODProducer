4ed8bc891797ab0aec4da71154624bcd87ab8ba2
rmastand <radha.mastandrea@gmail.com>
2018-11-26 13:45:18 -0500

    Update determine_validity_in_time.py

--- post/determine_validity_in_time.py
+++ post/determine_validity_in_time.py
@@ -1,24 +1,19 @@
+# only need valid, so go through the pare py stuff. use the plot_eff_lumi stuff
 
 import numpy as np
 import sys
-import csv
+import datetime
+import time
 
 
-run_alumi_file = sys.argv[1]
-lumibyls_file = sys.argv[2]
+plot_eff_lumi_file = sys.argv[1]
+lumibyls_file = sys.argv[3]
 
-# first: order all the lumiblocks in run A. they should be already ordered in this file
-runA_runs = []
-read_alumi_lines = open(run_alumi_file,"r").readlines()
-for line in read_alumi_lines[4:]:
-  char = line[0]
-  if char != "+":
-    runA_runs.append(line.split()[1].split(":")[0])
-  else:
-    break
-print "Done reading in ALumi"
 
-def read_lumi_by_ls(lumibyls_file):
+rev_ordered_triggers = ["HLT_Jet30","HLT_Jet60","HLT_Jet80","HLT_Jet110","HLT_Jet150","HLT_Jet190","HLT_Jet240","HLT_Jet300","HLT_Jet370"][::-1]
+
+
+def lumi_id_to_dmy(lumibyls_file):
 	"""
 	returns two dicts with keys = (run,lumiBlock)
 	1st values: gps times
@@ -28,10 +23,7 @@ def read_lumi_by_ls(lumibyls_file):
 	lines =  lumibyls.readlines()
 	split_lines = [line.split(",") for line in lines][2:]
 	char = ""
-	lumi_id_to_gps_times = {}
-	lumi_id_to_lumin = {}
-	time_series_all = []
-	lumin_all = []
+	lumi_id_to_date = {}
 	i = 0
 	while char !="#":
 		run = split_lines[i][0].split(":")[0]
@@ -41,33 +33,59 @@ def read_lumi_by_ls(lumibyls_file):
 		mdy = [int(x) for x in date.split("/")]
 		hms = [int(x) for x in tim.split(":")]
 		dt = datetime.datetime(mdy[2], mdy[0], mdy[1], hms[0], hms[1],hms[2])
-		lumi_id_to_gps_times[(run,lumi)] = time.mktime(dt.timetuple())
-		lumi_id_to_lumin[(run,lumi)] = (float(split_lines[i][5]),float(split_lines[i][6]))
-		time_series_all.append(time.mktime(dt.timetuple()))
-		lumin_all.append(float(split_lines[i][6]))
+		lumi_id_to_date[(run,lumi)] = str(mdy[1])+"/"+str(mdy[0])+"/"+str(mdy[2])
 		i += 1
 		try:
 			char = split_lines[i][0][0]
 		except: pass
-	return lumi_id_to_gps_times,lumi_id_to_lumin,time_series_all,lumin_all
-lumi_id_to_gps_times,lumi_id_to_lumin,time_series_all,lumin_all = read_lumi_by_ls(lumibyls_file)
+	return lumi_id_to_date
+lumi_id_to_date = lumi_id_to_dmy(lumibyls_file)
 
 
-ordered_triggers = ["HLT_Jet30","HLT_Jet60","HLT_Jet80","HLT_Jet110","HLT_Jet150","HLT_Jet190","HLT_Jet240","HLT_Jet300","HLT_Jet370"]
-trigger_vector_dict = {}
 
-all_lumis_A = []
-all_lumis_A_times = []
-for lumi_id in lumi_id_to_lumin.keys():
-		if lumi_id[0] in runA_runs:
-			all_lumis_A_times.append(lumi_id_to_gps_times[lumi_id])
-      all_lumis_A.append(lumi_id)
-      
-      # need to time order
- all_lumis_A_times_sorted,all_lumis_A_sorted = (list(t) for t in zip(*sorted(zip(all_lumis_A_times,all_lumis_A))))
+"""
+plot_eff_luminosity.txt:
+Total Integrated Luminosity, number index
+Total Integrated Luminosity, cumsum value
+Time ordered luminosity ids for all represented lumiblocks (use for x axis labels)
+For each trigger, going from BIGGEST TO SMALLEST:
+Effective Luminosity, number index
+Effective Luminosity, cumsum value
+"""
 
-# replicate each vector for each trigger as a line of 0s
-for trigger in ordered_triggers:
-  trigger_vector_dict[trigger] = np.zeros(len(all_lumis_A_sorted))
 
-# go thorugh the lumiblocks for each trigger, then index change to 1 if present
+
+eff_lumi_file =  open(plot_eff_lumi_file)
+lines = eff_lumi_file.readlines()
+# for the total luminosity file:
+
+# all point represent VALID lumiblocks, either in totla or for a given trigger
+runA_times = np.array([float(x) for x in lines[1].split(",")])
+runA_index = np.array(range(len(runA_lumin)))+1
+master_index = np.array([int(x) for x in lines[3].split(",")])+1
+master_lumin = np.array([float(x) for x in lines[4].split(",")])
+time_ordered_lumi_id = lines[5].split(",")
+
+#print np.logspace(min(master_index),max(master_index),num_samples)
+good_indices = np.logspace(np.log10(min(runA_index)),np.log10(max(runA_index)),num_samples).astype(int) -min(runA_index)
+
+plt.plot(np.take(runA_index,good_indices),np.take(runA_lumin,good_indices),"k",linewidth=9.0)
+
+
+
+for trig_index,trig in enumerate(rev_ordered_triggers):
+	print trig
+
+	index = np.array([int(x) for x in lines[3*trig_index+7].split(",")])+1
+
+	eff_lumin = np.array([float(x) for x in lines[3*trig_index+8].split(",")])
+	good_indices = np.logspace(np.log10(min(index)),np.log10(max(index)),num_samples).astype(int) - min(index)
+	print len(index), len(eff_lumin)
+
+        
+
+
+
+graph_eff_lumin()
+graph_eff_lumin_time_ordered()
+
