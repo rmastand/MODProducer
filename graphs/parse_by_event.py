dcde632208e5b85a4611ff9afb7c357208d32b30
rmastand <radha.mastandrea@gmail.com>
2019-01-08 13:53:30 -0500

    Create parse_by_event.py

---
+++ graphs/parse_by_event.py
@@ -0,0 +1,143 @@
+"""
+make a giant table. columns: event, lumiblock, trigger present (0 or 1), triggers fired, trigger prescale
+"""
+
+import numpy as np
+import datetime
+import time
+import sys
+import ast
+import os
+import csv
+
+"""generates file trig dicts, prints them to 1 dict for each file"""
+
+lumibyls_file = sys.argv[1]
+mod_file_inpur_dir = sys.argv[2]
+file_trig_dict_output_dir = sys.argv[3]
+
+def read_lumi_by_ls(lumibyls_file):
+	"""
+	returns two dicts with keys = (run,lumiBlock)
+	1st values: gps times
+	2nd values: (lumi_delivered, lumi_recorded)
+	"""
+	lumibyls = open(lumibyls_file)
+	lines =  lumibyls.readlines()
+	split_lines = [line.split(",") for line in lines][2:]
+	char = ""
+	lumi_id_to_gps_times = {}
+	lumi_id_to_lumin = {}
+	i = 0
+	while char !="#":
+		run = split_lines[i][0].split(":")[0]
+		lumi = split_lines[i][1].split(":")[0]
+		date = split_lines[i][2].split(" ")[0]
+		tim = split_lines[i][2].split(" ")[1]
+		mdy = [int(x) for x in date.split("/")]
+		hms = [int(x) for x in tim.split(":")]
+		dt = datetime.datetime(mdy[2], mdy[0], mdy[1], hms[0], hms[1],hms[2])
+		lumi_id_to_gps_times[(run,lumi)] = time.mktime(dt.timetuple())
+		lumi_id_to_lumin[(run,lumi)] = (float(split_lines[i][5]),float(split_lines[i][6]))
+		i += 1
+		try:
+			char = split_lines[i][0][0]
+		except: pass
+	return lumi_id_to_gps_times,lumi_id_to_lumin
+
+def is_lumi_valid(lumi_id, lumi_id_to_lumin):
+	"""
+	lumi_id = (run,lumiBlock)
+	"""
+	try:
+		luminosity = lumi_id_to_lumin[lumi_id]
+		return 1
+	except KeyError:
+		pass
+
+lumi_id_to_gps_times,lumi_id_to_lumin = read_lumi_by_ls(lumibyls_file)
+
+
+def read_mod_file(mod_file,file_trig_dict_output_dir,file_name,i,num_files):
+	"""
+	prints a dict of triggers FOR EACH MOD file
+	keys are triggers names (with versions)
+	subdicts are lists of good lumis the trigger was present for and the corresponding good prescale values
+	
+	each n correponds to a different trigger name
+	line n = uncut trigger name (with version)
+	line n+1 = list of good lumins ids trig_dict["good_lumis"]
+	line n+2 = list of good prescales trig_dict["good_prescales"]
+	line n+3 = list of lumi: # times fired paired. Comes from a dict where lumi id is the key and # times fired is the value trig_dict["fired"]
+	"""
+	trig_dict = {}
+
+	with open(mod_file) as file:
+		
+		for line in file:
+			# MOST CODE TAKEN FROM GET_TRIGGER_INFO.py
+			# keeps track of the run, lumiBlock
+			# this should signal each separate event
+			if ("Cond" in line.split()) and ("#" not in line.split()):
+				run,lumiBlock = line.split()[1],line.split()[3]
+
+			if ("Trig" in line.split()) and ("#" not in line.split()):
+				# all within 1 event
+				# given line: [Trig identifier, trig name, prescale1, prescale2, fired?]
+				if line.split()[1] not in trig_dict.keys():
+					trig_dict[line.split()[1]] = {
+								      "good_lumis":[],
+								      # corresponds to the prescale for a given GOOD LUMI BLOCK
+								      "good_prescales":[],
+									  "fired":{}
+
+								     }
+
+					# if the lumi block is valid and if not alredy been analyzed (so present by definition)
+					if is_lumi_valid((run,lumiBlock),lumi_id_to_lumin):
+						if (run,lumiBlock) not in trig_dict[line.split()[1]]["good_lumis"]:
+							trig_dict[line.split()[1]]["good_lumis"].append((run,lumiBlock))
+							trig_dict[line.split()[1]]["good_prescales"].append(float(line.split()[2])*float(line.split()[3]))
+						try:
+							trig_dict[line.split()[1]]["fired"][(run,lumiBlock)]+= int(line.split()[4])
+						except KeyError:
+							trig_dict[line.split()[1]]["fired"][(run,lumiBlock)] = int(line.split()[4])
+				else:
+
+					if is_lumi_valid((run,lumiBlock),lumi_id_to_lumin):
+						if (run,lumiBlock) not in trig_dict[line.split()[1]]["good_lumis"]:
+							trig_dict[line.split()[1]]["good_lumis"].append((run,lumiBlock))
+							trig_dict[line.split()[1]]["good_prescales"].append(float(line.split()[2])*float(line.split()[3]))
+						try:
+							trig_dict[line.split()[1]]["fired"][(run,lumiBlock)]+= int(line.split()[4])
+						except KeyError:
+							trig_dict[line.split()[1]]["fired"][(run,lumiBlock)] = int(line.split()[4])
+
+		with open(file_trig_dict_output_dir+file_name.replace(".mod",".txt"), "w") as output:
+			writer = csv.writer(output, lineterminator='\n')
+			for trigger in trig_dict:
+				writer.writerow(["# "+trigger]) 
+				good_lumis = []
+				for lumi in trig_dict[trigger]["good_lumis"]:
+					good_lumis.append(lumi[0]+":"+lumi[1])
+				writer.writerow(good_lumis)
+				writer.writerow(trig_dict[trigger]["good_prescales"])
+				fired = []
+				for lumi_id in trig_dict[trigger]["fired"].keys():
+				
+					fired.append(lumi_id[0]+"_"+lumi_id[1]+":"+str(trig_dict[trigger]["fired"][lumi_id]))
+				writer.writerow(fired)
+		return trig_dict
+
+
+i = 1
+num_files = len(os.listdir(mod_file_inpur_dir))
+for file in os.listdir(mod_file_inpur_dir):
+	# if file has not already been processed
+	if file.replace(".mod",".txt") not in os.listdir(file_trig_dict_output_dir):
+		print "Processing file " + file + ", File "+str(i)+" of " + str(num_files)
+		file_trig_dict = read_mod_file(mod_file_inpur_dir+"/"+file,file_trig_dict_output_dir,file,i,num_files)
+	else:
+		print "Already processed " + file + ", File "+str(i)+" of " + str(num_files)
+	i += 1
+	
