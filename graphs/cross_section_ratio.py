7b06240cd57e0bf2d01744d4e0328181f0f34778
rmastand <radha.mastandrea@gmail.com>
2019-01-09 15:46:19 -0500

    Update cross_section_ratio.py

--- graphs/cross_section_ratio.py
+++ graphs/cross_section_ratio.py
@@ -9,11 +9,18 @@ to answer the question: look at events where lower fired, higher was present. wh
 for which 240 fired, ask for those events, when did 300 fire? that can’t be above 1
 """
 
+lower_trig_name = "HLT_Jet240"
+higher_trig_name = "HLT_Jet300"
+
+
 # YIKES these names...sorry to whoeever who reads this next lol
 num_lower_fired_and_higher_present = 0
 num_higher_fired_given_lower_fired_and_higher_present = 0
 
 with open(parsed_by_event,"r") as event_listing:
   for line in event_listing:
-    print line
+    if "EventNum" not in line.split(): #just ignores the top line
+      print line.split()[3].split(",")
+      print line.split()[5].split(",")
+      
 
