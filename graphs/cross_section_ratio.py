f6ffd99743b0fc12d85683dab77f5242173ddc14
rmastand <radha.mastandrea@gmail.com>
2019-01-09 15:33:29 -0500

    Create cross_section_ratio.py

---
+++ graphs/cross_section_ratio.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sys
+
+parsed_by_event = sys.argv[1]
+
+"""
+to answer the question: look at events where lower fired, higher was present. what fraction of the time did higher fire?
+for which 240 fired, ask for those events, when did 300 fire? that can’t be above 1
+"""
