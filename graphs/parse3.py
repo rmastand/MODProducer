79865676b2ae43758df0a5b4194e6ee00d060248
rmastand <radha.mastandrea@gmail.com>
2019-01-09 23:20:30 -0500

    Update parse3.py

--- graphs/parse3.py
+++ graphs/parse3.py
@@ -181,14 +181,12 @@ writer = csv.writer(output_3, lineterminator='\n')
 print "cross lumsection as time" 
 for trigger in rev_ordered_triggers:
   trigger_cross_section = []
-  ttimes = []
   for i,eff_lumin in enumerate(trigger_eff_lumis_dict[trigger]):
     try:
     	trigger_cross_section.append(float(eff_lumin)/trigger_eff_fired_dict[trigger][i])
-	ttimes.append(trigger_times_dict[trigger][i])
     except ZeroDivisionError:
-	pass
-  trigger_times_sorted,trigger_cross_section_sorted = (list(t) for t in zip(*sorted(zip(ttimes,trigger_cross_section))))
+	trigger_cross_section.append(0.)
+  trigger_times_sorted,trigger_cross_section_sorted = (list(t) for t in zip(*sorted(zip(trigger_times_dict[trigger],trigger_cross_section))))
   writer.writerow(trigger_times_sorted)  
   writer.writerow(trigger_cross_section_sorted)  
   
@@ -211,16 +209,14 @@ for trigger in rev_ordered_triggers:
     if time in trigger_times_dict[trigger]:
       trigger_ids.append(id)
   trigger_cross_section = []
-  iids = []
   for i,eff_lumin in enumerate(trigger_eff_lumis_dict[trigger]):
         try:
     		trigger_cross_section.append(float(eff_lumin)/trigger_eff_fired_dict[trigger][i])
-		iids.append(trigger_ids[i])
-        except ZeroDivisionError:
-		pass
-  writer.writerow(iids)  
+    	except ZeroDivisionError:
+		trigger_cross_section.append(0.)
+  writer.writerow(trigger_ids)  
   writer.writerow(trigger_cross_section_sorted)  
-  print len(iids)
+  print len(trigger_ids)
   print len(trigger_cross_section_sorted)
 output_4.close()
 
