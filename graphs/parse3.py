3f05bd5d2d710703e83bbe7a442f9afe2103bfcb
rmastand <radha.mastandrea@gmail.com>
2019-01-09 21:51:22 -0500

    Update parse3.py

--- graphs/parse3.py
+++ graphs/parse3.py
@@ -77,7 +77,7 @@ with open(by_event_2_file, "r") as input:
     elif i == 1: # lumi ids
       lumi_ids = line.split(",")[:-1]
       lumi_ids = [(x.split(":")[0],x.split(":")[1]) for x in lumi_ids]
-      trigger_ids_dict[trigger] = lumi_ids
+      trigger_lumi_ids_dict[trigger] = lumi_ids
     elif i == 2: # eff lumis
       eff_lumis = line.split(",")[:-1]
       eff_lumis = [float(x) for x in eff_lumis]
