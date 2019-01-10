5ca42792f4e9fdc657380e438ce6cb760b2e6ffb
rmastand <radha.mastandrea@gmail.com>
2019-01-09 22:42:55 -0500

    Update plot2.py

--- graphs/plot2.py
+++ graphs/plot2.py
@@ -135,15 +135,15 @@ def graph_eff_lumin():
 
 	for trig_index,trig in enumerate(rev_ordered_triggers):
 	        print trig
-		print lines[0][:20]
-		print lines[1][:20]
-		print lines[2][:20]
-		print lines[3][:20]
-		print lines[4][:20]
-		print lines[2*trig_index+3]
-		index = np.array([int(x) for x in lines[2*trig_index+3].split(",")[:-1]])
+		#print lines[0][:20]
+		#print lines[1][:20]
+		#print lines[2][:20]
+		#print lines[3][:20]
+		#print lines[4][:20]
+		#print lines[2*trig_index+3]
+		index = np.array([int(x) for x in lines[2*trig_index+3].split(",")[1:-1]])
 
-		eff_lumin = np.array([float(x) for x in lines[2*trig_index+4].split(",")[:-1]])
+		eff_lumin = np.array([float(x) for x in lines[2*trig_index+4].split(",")[1:-1]])
                 good_indices = np.logspace(np.log10(min(index)),np.log10(max(index)),num_samples).astype(int) #- min(index)
                 print len(index), len(eff_lumin)
 
