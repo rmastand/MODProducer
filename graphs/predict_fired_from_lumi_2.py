47046dfeab6017c5fb6b29e9956d7a65c88b793e
rmastand <radha.mastandrea@gmail.com>
2019-01-11 10:10:24 -0500

    Update predict_fired_from_lumi_2.py

--- graphs/predict_fired_from_lumi_2.py
+++ graphs/predict_fired_from_lumi_2.py
@@ -67,10 +67,14 @@ with open(parsed_by_event,"r") as event_listing:
 
 			for j, present_trigger in enumerate(triggers_present):
 			  if present_trigger in rev_ordered_triggers:
-			    if lumi_id not in master_dict[present_trigger].keys():
-			      master_dict[present_trigger][lumi_id] = {"prescale":prescales[j],"times_fired":0}		  
-			    if present_trigger in triggers_fired:   
-			      master_dict[present_trigger][lumi_id]["times_fired"] += 1
+				try:
+					if present_trigger in triggers_fired:   
+			     			master_dict[present_trigger][lumi_id]["times_fired"] += 1
+				except KeyError:
+			      		master_dict[present_trigger][lumi_id] = {"prescale":prescales[j],"times_fired":0}
+					if present_trigger in triggers_fired:   
+			     			master_dict[present_trigger][lumi_id]["times_fired"] += 1
+			    
 
 
 
