c3f676242451f22e429fb28cc26b85c328c52a62
rmastand <radha.mastandrea@gmail.com>
2019-01-15 10:45:52 -0500

    Update plot2.py

--- graphs/plot2.py
+++ graphs/plot2.py
@@ -144,7 +144,7 @@ def graph_eff_lumin():
 		index = np.array([int(x) for x in lines[2*trig_index+3].split(",")[1:-1]])
 
 		eff_lumin = np.cumsum(np.array([float(x) for x in lines[2*trig_index+4].split(",")[1:-1]]))
-                good_indices = np.logspace(np.log10(min(index)),np.log10(max(index)),num_samples).astype(int) #- min(index)
+                good_indices = np.logspace(np.log10(min(index)),np.log10(max(index)),num_samples).astype(int) - min(index)
                 print len(index), len(eff_lumin)
 		
 		g_i = []
@@ -203,7 +203,7 @@ def graph_eff_lumin_time_ordered():
 	time_ordered_lumi_id = lines[0].split(",")
 
         #print np.logspace(min(master_index),max(master_index),num_samples)
-        good_indices = np.linspace(min(runA_index),max(runA_index),num_samples).astype(int) - min(runA_index)
+        good_indices = np.linspace(min(runA_index),max(runA_index),num_samples).astype(int) #- min(runA_index)
 	
 	g_i = []
 	for m,ind in enumerate(runA_index):
