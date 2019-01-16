ae49e4a3d6b1b8719008d90108b69818c07d6d45
rmastand <radha.mastandrea@gmail.com>
2019-01-16 15:10:06 -0500

    Update plot2.py

--- graphs/plot2.py
+++ graphs/plot2.py
@@ -143,11 +143,11 @@ def graph_eff_lumin():
 			print trig
 
 
-			index = np.array([int(x) for x in lines[2*trig_index+3].split(",")])
+			index = np.array([int(x) for x in lines[2*trig_index+3].split(",")])[1:]
 			
 			print index[-100:]
 
-			eff_lumin = np.cumsum(np.array([float(x) for x in lines[2*trig_index+4].split(",")]))
+			eff_lumin = np.cumsum(np.array([float(x) for x in lines[2*trig_index+4].split(",")]))[1:]
 			good_indices = np.logspace(np.log10(min(index)),np.log10(max(index)),num_samples).astype(int)# - min(index)
 			print len(index), len(eff_lumin)
 
