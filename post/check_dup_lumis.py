ab30ffa02a628b0f9cca93259e0b01359f628044
rmastand <radha.mastandrea@gmail.com>
2019-01-23 14:42:36 -0500

    Update check_dup_lumis.py

--- post/check_dup_lumis.py
+++ post/check_dup_lumis.py
@@ -11,11 +11,11 @@ import csv
 lumibyls_file = sys.argv[1]
 #mod_file_inpur_dir = sys.argv[2]
 
-all_dirs = ["/Volumes/Seagate Backup Plus Drive/MITOpenDataProject/eos/opendata/cms/Run2011A/Jet/MOD/12Oct2013-v1/10000/",
-	   "/Volumes/Seagate Backup Plus Drive/MITOpenDataProject/eos/opendata/cms/Run2011A/Jet/MOD/12Oct2013-v1/20000_a/",
-	   "/Volumes/Seagate Backup Plus Drive/MITOpenDataProject/eos/opendata/cms/Run2011A/Jet/MOD/12Oct2013-v1/20000_b/",
-	   "/Volumes/Seagate Backup Plus Drive/MITOpenDataProject/eos/opendata/cms/Run2011A/Jet/MOD/12Oct2013-v1/20000_c/",
-	   "/Volumes/Seagate Backup Plus Drive/MITOpenDataProject/eos/opendata/cms/Run2011A/Jet/MOD/12Oct2013-v1/20001/"]
+all_dirs = ["/Volumes/Seagate Backup Plus Drive/MITOpenDataProject/eos/opendata/cms/Run2011A/Jet/stats/12Oct2013-v1/10000/",
+	   "/Volumes/Seagate Backup Plus Drive/MITOpenDataProject/eos/opendata/cms/Run2011A/Jet/stats/12Oct2013-v1/20000_a/",
+	   "/Volumes/Seagate Backup Plus Drive/MITOpenDataProject/eos/opendata/cms/Run2011A/Jet/stats/12Oct2013-v1/20000_b/",
+	   "/Volumes/Seagate Backup Plus Drive/MITOpenDataProject/eos/opendata/cms/Run2011A/Jet/stats/12Oct2013-v1/20000_c/",
+	   "/Volumes/Seagate Backup Plus Drive/MITOpenDataProject/eos/opendata/cms/Run2011A/Jet/stats/12Oct2013-v1/20001/"]
 
 
 def read_lumi_by_ls(lumibyls_file):
@@ -60,7 +60,7 @@ def is_lumi_valid(lumi_id, lumi_id_to_lumin):
 lumi_id_to_gps_times,lumi_id_to_lumin = read_lumi_by_ls(lumibyls_file)
 
 
-def read_mod_file(mod_file,file_name,i,num_files):
+def read_stats_file(stats_file,file_name,i,num_files):
 	"""
 	prints a dict of triggers FOR EACH MOD file
 	keys are triggers names (with versions)
@@ -73,33 +73,67 @@ def read_mod_file(mod_file,file_name,i,num_files):
 	line n+3 = list of lumi: # times fired paired. Comes from a dict where lumi id is the key and # times fired is the value trig_dict["fired"]
 	"""
 	valid_lumis = []
+	all_lumis = []
+	num_valid_events = 0
+	num_total_events = 0
 
-	with open(mod_file) as file:
+	with open(stats_file) as file:
 		
 		for line in file:
 			# MOST CODE TAKEN FROM GET_TRIGGER_INFO.py
 			# keeps track of the run, lumiBlock
 			# this should signal each separate event
-			if ("Cond" in line.split()) and ("#" not in line.split()):
-				run,lumiBlock = line.split()[1],line.split()[3]
+			if ("File" in line.split()) and ("#" not in line.split()):
+				num_valid_events,num_total_events = line.split()[3], line.split()[2]
+			if ("Block" in line.split()) and ("#" not in line.split()):	
+				run,lumiBlock = line.split()[1],line.split()[2]
+				all_lumis.append(float(run+"."+lumiBlock))
 				if is_lumi_valid((run,lumiBlock),lumi_id_to_lumin):
-					if float(run+"."+lumiBlock) not in valid_lumis:
 						valid_lumis.append(float(run+"."+lumiBlock))
-	return valid_lumis
+	return valid_lumis,all_lumis,num_valid_events,num_total_events
 
 		
 all_valid_lumis = []
+all_num_events_valid = []
+all_num_events_total = []
+all_num_lumis_valid = []
+all_num_lumis_total = []
 i = 1
 num_files = 1223
 for dire in all_dirs:
 	for file in os.listdir(dire):
 		# if file has not already been processed
 		print "Processing file " + file + ", File "+str(i)+" of " + str(num_files)
-		valid_lumis = read_mod_file(dire+"/"+file,file,i,num_files)
+		valid_lumis,all_lumis,num_valid_events,num_total_events = read_stats_file(dire+"/"+file,file,i,num_files)
 		all_valid_lumis += valid_lumis
 		i += 1
-
-
-print len(set([x for x in all_valid_lumis if all_valid_lumis.count(x) > 1]))
-print len(all_valid_lumis)
+		all_num_events_valid.append(num_valid_events)
+		all_num_events_total.append(num_total_events)
+		all_num_lumis_valid.append(len(valid_lumis))
+		all_num_lumis_total.append(len(all_lumis))
+print "all lumis duplicated, total lumis"
+print len(set([x for x in all_valid_lumis if all_valid_lumis.count(x) > 1])),len(all_valid_lumis)
 #print set([x for x in all_valid_lumis if all_valid_lumis.count(x) > 1])
+
+plt.figure()
+plt.hist(all_num_events_valid)
+plt.title("all_num_events_valid")
+plt.show()
+
+plt.figure()
+plt.hist(all_num_events_total)
+plt.title("all_num_events_total")
+plt.show()
+
+plt.figure()
+plt.hist(all_num_lumis_valid)
+plt.title("all_num_lumis_valid")
+plt.show()
+
+plt.figure()
+plt.hist(all_num_lumis_total)
+plt.title("all_num_lumis_total")
+plt.show()
+
+
+
