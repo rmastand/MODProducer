4fc341c325e5112add309b0bb17300f72f39e6cf
rmastand <radha.mastandrea@gmail.com>
2019-02-13 10:35:14 -0500

    Update count.py

--- post/count.py
+++ post/count.py
@@ -5,7 +5,7 @@ file = sys.argv[1]
 
 total_lumin = 0
 
-for line in open(file,"r").readlines():
+for line in open(file,"r").readlines()[1:]:
   rec_lumi = line.split()[3]
   total_lumin += float(rec_lumi)
   
