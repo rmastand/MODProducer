66e97cc9c388e08fdacabb274d984979ec5de282
rmastand <radha.mastandrea@gmail.com>
2019-03-02 10:23:00 -0500

    Update fill_event_count_table.py

--- post/fill_event_count_table.py
+++ post/fill_event_count_table.py
@@ -60,7 +60,9 @@ with open(output_file,"w") as output:
     output.write(str(object)+ " & "+str(pdg_id_order_names[i])+" & " +str(all_pfcs[object])+" & " +str(all_pfcs_1Gev[object])+" & " +"\\\ \n")
   output.write("\hline\n")
   for i, object in enumerate(pdg_id_order_2):
-    output.write(str(object)+ " & "+str(pdg_id_order_2_names[i])+" & " +str(all_pfcs[object])+" & " +str(all_pfcs_1Gev[object])+" & " +"\\\ \n")
+	try: output.write(str(object)+ " & "+str(pdg_id_order_2_names[i])+" & " +str(all_pfcs[object])+" & " +str(all_pfcs_1Gev[object])+" & " +"\\\ \n")
+  	except KeyError: output.write(str(object)+ " & "+str(pdg_id_order_2_names[i])+" & " +str(all_pfcs[object])+" & " +str(0)+" & " +"\\\ \n")
+
   output.write("\hline\n")
   output.write("\hline\n")
   output.write("\end{tabular} \n")
