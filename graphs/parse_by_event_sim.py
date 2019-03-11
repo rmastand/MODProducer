02fdebfbed572d0dd253b829ea96a6901154e817
rmastand <radha.mastandrea@gmail.com>
2019-03-10 22:06:12 -0400

    Update parse_by_event_sim.py

--- graphs/parse_by_event_sim.py
+++ graphs/parse_by_event_sim.py
@@ -31,7 +31,7 @@ all_MOD_dirs = ["/Volumes/Drive/MITOpenLow/eos/opendata/cms/MonteCarlo2011/Summe
 	      ]
 """
 
-all_MOD_dirs = ["/Volumes/SimDataHighPt/MITOpenHigh/eos/opendata/cms/MonteCarlo2011/Summer11LegDR/QCD_Pt-300to470_TuneZ2_7Tev_pythia6/MOD/PU_S13_START53_LV6-v1/00001/",
+all_MOD_dirs = [
 		"/Volumes/SimDataHighPt/MITOpenHigh/eos/opendata/cms/MonteCarlo2011/Summer11LegDR/QCD_Pt-470to600_TuneZ2_7Tev_pythia6/MOD/PU_S13_START53_LV6-v1/00000/",
 		"/Volumes/SimDataHighPt/MITOpenHigh/eos/opendata/cms/MonteCarlo2011/Summer11LegDR/QCD_Pt-600to800_TuneZ2_7Tev_pythia6/MOD/PU_S13_START53_LV6-v1/00000/",
 		"/Volumes/SimDataHighPt/MITOpenHigh/eos/opendata/cms/MonteCarlo2011/Summer11LegDR/QCD_Pt-800to1000_TuneZ2_7Tev_pythia6/MOD/PU_S13_START53_LV6-v1/00000/",
@@ -44,8 +44,7 @@ all_MOD_dirs = ["/Volumes/SimDataHighPt/MITOpenHigh/eos/opendata/cms/MonteCarlo2
 all_MOD_dir_labels = ["0to5_","5to15_","15to30_","30to50_","50to80_","80to120_","120to170_","170to300_","300to470_",
 		     "470to600_","600to800_","800to1000_","1000to1400_","1400to1800_","1800_"]
 """
-all_MOD_dir_labels = ["300to470_",
-		     "470to600_","600to800_","800to1000_","1000to1400_","1400to1800_","1800_"]
+all_MOD_dir_labels = ["470to600_","600to800_","800to1000_","1000to1400_","1400to1800_","1800_"]
 output_extension = sys.argv[1]
 folder = sys.argv[2]
 
