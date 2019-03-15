dd8452380e91e7b185ec382f971221510d57134c
rmastand <radha.mastandrea@gmail.com>
2019-03-15 00:46:32 -0700

    Update fill_event_count_table.py

--- post/fill_event_count_table.py
+++ post/fill_event_count_table.py
@@ -11,36 +11,37 @@ output_file = sys.argv[1]
 
 jet_is_good = False
 
-all_dirs = ["/Volumes/Seagate Backup Plus Drive/MODAnalyzerResults/2011_pfc_dat_files/10000/",
-	   "/Volumes/Seagate Backup Plus Drive/MODAnalyzerResults/2011_pfc_dat_files/20000_a/",
-	   "/Volumes/Seagate Backup Plus Drive/MODAnalyzerResults/2011_pfc_dat_files/20000_b/",
-	   "/Volumes/Seagate Backup Plus Drive/MODAnalyzerResults/2011_pfc_dat_files/20000_c/",
-	   "/Volumes/Seagate Backup Plus Drive/MODAnalyzerResults/2011_pfc_dat_files/20001/"]
+all_dirs = ["/Volumes/Seagate Backup Plus Drive/MODAnalyzerResults/ALL_FILES/10000/",
+	   "/Volumes/Seagate Backup Plus Drive/MODAnalyzerResults/ALL_FILES/20000_a/",
+	   "/Volumes/Seagate Backup Plus Drive/MODAnalyzerResults/ALL_FILES/20000_b/",
+	   "/Volumes/Seagate Backup Plus Drive/MODAnalyzerResults/ALL_FILES/20000_c/",
+	   "/Volumes/Seagate Backup Plus Drive/MODAnalyzerResults/ALL_FILES/20001/"]
 
 l = 0
 for dat_dir in all_dirs:
 	for dat_file in os.listdir(dat_dir):
-	    with open(dat_dir+"/"+dat_file, "r") as file:
-	      l += 1
-	      print l, 1223
-	      for line in file:
-		if ("BeginJetProperties" in line.split()) and ("#" not in line.split()):
-		  jet_pt = float(line.split()[2])
-		  jet_eta = float(line.split()[4])
-		  if (jet_pt > 375) and (-1.9 < jet_eta < 1.9):
-		    jet_is_good = True
-		  else: jet_is_good = False
+		if "particles" in dat_file:
+		    with open(dat_dir+"/"+dat_file, "r") as file:
+		      l += 1
+		      print l, 1223
+		      for line in file:
+			if ("BeginJetProperties" in line.split()) and ("#" not in line.split()):
+			  jet_pt = float(line.split()[2])
+			  jet_eta = float(line.split()[4])
+			  if (jet_pt > 375) and (-1.9 < jet_eta < 1.9):
+			    jet_is_good = True
+			  else: jet_is_good = False
 
-		if ("BeginParticle" in line.split()) and ("#" not in line.split()):
-		  if jet_is_good:
-			  particle_pt = float(line.split()[1])
-			  particle_id = int(line.split()[3])
-			  try: all_pfcs[particle_id] += 1
-			  except KeyError: all_pfcs[particle_id] = 1
-			  if particle_pt > 1:
-			    try: all_pfcs_1Gev[particle_id] += 1
-			    except KeyError: all_pfcs_1Gev[particle_id] = 1
-          
+			if ("BeginParticle" in line.split()) and ("#" not in line.split()):
+			  if jet_is_good:
+				  particle_pt = float(line.split()[1])
+				  particle_id = int(line.split()[3])
+				  try: all_pfcs[particle_id] += 1
+				  except KeyError: all_pfcs[particle_id] = 1
+				  if particle_pt > 1:
+				    try: all_pfcs_1Gev[particle_id] += 1
+				    except KeyError: all_pfcs_1Gev[particle_id] = 1
+
 
 pdg_id_order = [11, -11, 13, -13, 211, -211]
 pdg_id_order_names = ["Electron (e$^-$)", "Positron (e$^+$)", "Muon ($\mu^-$)", "Antimuon ($\mu^+$)", "Positive Hadron ($\pi^+$)", "Negative Hadron ($\pi^-$)"]
